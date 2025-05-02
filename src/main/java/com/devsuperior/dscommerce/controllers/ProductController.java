1d32f1837aa2088f1db2a63abbb8ad6e496337d1
audreyrdsc <audreyrdsc@gmail.com>
2025-05-02 15:08:48 -0300

    Incluindo o ResponseEntity no Controller

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -5,8 +5,11 @@ import com.devsuperior.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -17,20 +20,42 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping(value = "/{id}")
+    public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
+        ProductDTO dto = productService.findById(id);
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable) {
+        Page<ProductDTO> dto = productService.findAll(pageable);
+        return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
+        dto = productService.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+}
+
+    /* SEM RESPONSE ENTITY
+        @GetMapping(value = "/{id}")
     public ProductDTO findById(@PathVariable Long id) {
         ProductDTO dto = productService.findById(id);
         return dto;
     }
 
-    @GetMapping
+        @GetMapping
     public Page<ProductDTO> findAll(Pageable pageable) {
         return productService.findAll(pageable);
     }
 
-    @PostMapping
+        @PostMapping
     public ProductDTO insert(@RequestBody ProductDTO dto) {
         dto = productService.insert(dto);
         return dto;
     }
-
-}
+     */
